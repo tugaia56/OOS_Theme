3193849632bea87b46ff3ccd19dd1009897184d0
Tullio <tugaia56#gmail.com.>
2026-01-12 17:19:07 +0100

    Version update: Android 15 - 16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,9 +49,9 @@ android {
     defaultConfig {
         applicationId="tugaia56.dark.shadow.theme"
         minSdk=28
-        versionCode = 630
-        versionName = "6.3.0"
-        setProperty("archivesBaseName", "Dark_Shadow_Theme_v6.3.0")
+        versionCode = 640
+        versionName = "6.4.0"
+        setProperty("archivesBaseName", "Dark_Shadow_Theme_v6.4.0")
 
         // Themers: DO NOT MODIFY
         buildConfigField("boolean", "SUPPORTS_THIRD_PARTY_SYSTEMS", "$SUPPORTS_THIRD_PARTY_SYSTEMS")
