67a7baafb7415445929b0960b736e4a3737569c1
Tullio <tugaia56#gmail.com.>
2026-04-03 09:56:31 +0200

    Version update: Release 7.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,8 +49,8 @@ android {
     defaultConfig {
         applicationId="tugaia56.dark.shadow.theme"
         minSdk=33
-        versionCode = 690
-        versionName = "6.9.0"
+        versionCode =700
+        versionName = "7.0.0"
 
         // Themers: DO NOT MODIFY
         buildConfigField("boolean", "SUPPORTS_THIRD_PARTY_SYSTEMS", "$SUPPORTS_THIRD_PARTY_SYSTEMS")
